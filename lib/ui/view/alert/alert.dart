7991ee78c4b1de7995e73dd4ebd761400a8e9d85
= <=>
2023-11-17 14:50:32 +0700

    mlasst version :)))

--- lib/ui/view/alert/alert.dart
+++ lib/ui/view/alert/alert.dart
@@ -332,22 +332,3 @@ Container NewsCardItem(context) {
         ),
       ));
 }
-
-class PreLoading extends StatelessWidget {
-  const PreLoading({
-    super.key,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      color: Color.fromRGBO(9, 98, 169, 1),
-      child: const Center(
-        child: CircularProgressIndicator(
-          valueColor: AlwaysStoppedAnimation<Color>(Colors.blue),
-          strokeWidth: 2.0,
-        ),
-      ),
-    );
-  }
-}
--- lib/ui/widget/pre_loading.dart
+++ lib/ui/widget/pre_loading.dart
@@ -7,23 +7,28 @@ class PreLoading extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Color.fromRGBO(9, 98, 169, 1),
-      child: const Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Center(
-            child: CircularProgressIndicator(
-              valueColor: AlwaysStoppedAnimation<Color>(Colors.blue),
-              strokeWidth: 2.0,
+    return Scaffold(
+      body: Container(
+        color: Color.fromRGBO(9, 98, 169, 1),
+        child: const Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Center(
+              child: CircularProgressIndicator(
+                valueColor: AlwaysStoppedAnimation<Color>(Colors.blue),
+                strokeWidth: 2.0,
+              ),
             ),
-          ),
-          Text(
-            'Đang tải, vui lòng đợi...',
-            style: TextStyle(color: Colors.white, fontSize: 12),
-          )
-        ],
+            SizedBox(
+              height: 20,
+            ),
+            Text(
+              'Đang tải dữ liệu, vui lòng đợi...',
+              style: TextStyle(color: Colors.white, fontSize: 14),
+            )
+          ],
+        ),
       ),
     );
   }
